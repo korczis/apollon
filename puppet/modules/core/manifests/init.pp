822595ed7db0a9c6f388a0ce655f6c71b655f9c6
Tomas Korcak <korczis@gmail.com>
2014-05-09 23:23:51 -0700

    Proper dependencies ...

--- puppet/modules/core/manifests/init.pp
+++ puppet/modules/core/manifests/init.pp
@@ -3,11 +3,17 @@
 # Installs core packages
 #
 class core {
-	exec { "apt-update":
-	    command => "/usr/bin/apt-get update",
-	}
+  exec { "add-webupd8team-java-ppa":
+    command => "/usr/bin/add-apt-repository ppa:webupd8team/java",
+  }
 
-	exec { "apt-upgrade":
-	    command => "/usr/bin/apt-get upgrade -y",
-	}
+  exec { "apt-update":
+    require => Exec['add-webupd8team-java-ppa'],
+    command => "/usr/bin/apt-get update",
+  }
+
+  exec { "apt-upgrade":
+    require => Exec['apt-update'],
+    command => "/usr/bin/apt-get upgrade -y",
+  }
 }
