eedd6582bc1279396a048462ad33f07bd0efb2d3
Tomas Korcak <korczis@gmail.com>
2014-05-09 23:22:22 -0700

    Added configuration files

--- puppet/modules/postgresql/manifests/init.pp
+++ puppet/modules/postgresql/manifests/init.pp
@@ -11,6 +11,64 @@ class postgresql {
 
   service { 'postgresql':
     ensure  => running,
-    require => Package['postgresql'];
+    require => [
+      Package['postgresql'],
+      File['/etc/postgresql/9.3/main/environment'],
+      File['/etc/postgresql/9.3/main/pg_ctl.conf'],
+      File['/etc/postgresql/9.3/main/pg_hba.conf'],
+      File['/etc/postgresql/9.3/main/pg_ident.conf'],
+      File['/etc/postgresql/9.3/main/postgresql.conf'],
+      File['/etc/postgresql/9.3/main/start.conf'],
+    ];
+  }
+
+  file { '/etc/postgresql/9.3/main/environment':
+    owner => postgres,
+    group => postgres,
+    source  => 'puppet:///modules/postgresql/9.3/main/environment',
+    require => Package['postgresql'],
+    notify  => Service['postgresql'];
+  }
+
+  file { '/etc/postgresql/9.3/main/pg_ctl.conf':
+    owner => postgres,
+    group => postgres,
+    source  => 'puppet:///modules/postgresql/9.3/main/pg_ctl.conf',
+    require => Package['postgresql'],
+    notify  => Service['postgresql'];
+  }
+
+  file { '/etc/postgresql/9.3/main/pg_hba.conf':
+    owner => postgres,
+    group => postgres,
+    mode => 0640,
+    source  => 'puppet:///modules/postgresql/9.3/main/pg_hba.conf',
+    require => Package['postgresql'],
+    notify  => Service['postgresql'];
+  }
+
+  file { '/etc/postgresql/9.3/main/pg_ident.conf':
+    owner => postgres,
+    group => postgres,
+    mode => 0640,
+    source  => 'puppet:///modules/postgresql/9.3/main/pg_ident.conf',
+    require => Package['postgresql'],
+    notify  => Service['postgresql'];
+  }
+
+  file { '/etc/postgresql/9.3/main/postgresql.conf':
+    owner => postgres,
+    group => postgres,
+    source  => 'puppet:///modules/postgresql/9.3/main/postgresql.conf',
+    require => Package['postgresql'],
+    notify  => Service['postgresql'];
+  }
+
+  file { '/etc/postgresql/9.3/main/start.conf':
+    owner => postgres,
+    group => postgres,
+    source  => 'puppet:///modules/postgresql/9.3/main/start.conf',
+    require => Package['postgresql'],
+    notify  => Service['postgresql'];
   }
 }
