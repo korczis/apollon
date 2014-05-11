b8bdcd4403acd9617dfc384916ba24d8fb609c1d
Tomas Korcak <korczis@gmail.com>
2014-05-10 17:42:23 -0700

    Create some directories (scripts/tmp) and echo that we are done

--- puppet/modules/core/manifests/init.pp
+++ puppet/modules/core/manifests/init.pp
@@ -3,6 +3,16 @@
 # Installs core packages
 #
 class core {
+  # create a directory for vagrant scripts
+  file { "/home/vagrant/scripts":
+    ensure => "directory",
+  }
+
+  # create a directory for temporary files scripts
+  file { "/home/vagrant/tmp":
+    ensure => "directory",
+  }
+
   exec { "add-webupd8team-java-ppa":
     command => "/usr/bin/add-apt-repository ppa:webupd8team/java",
   }
@@ -15,5 +25,14 @@ class core {
   exec { "apt-upgrade":
     require => Exec['apt-update'],
     command => "/usr/bin/apt-get upgrade -y",
+  }
+
+  exec { "core-done":
+    require => [
+      Exec['apt-upgrade'],
+      File['/home/vagrant/tmp'],
+      File['/home/vagrant/scripts']
+    ],
+    command => "/bin/echo Core Init done",
   }
 }
