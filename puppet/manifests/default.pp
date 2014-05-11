44af63fe69fda67ffd58e2f5401ca209ca5ab84a
Tomas Korcak <korczis@gmail.com>
2014-05-10 17:42:08 -0700

    Modules split

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,8 +1,28 @@
 node default {
   include core
+}
+
+node all {
   include db::all
   include dev
   include ops
+  include rabbitmq
   include science
+  include web::all
+}
+
+node dev {
+  include dev
+}
+
+node db {
+  include db::all
+}
+
+node ops {
+  include ops
+}
+
+node web {
   include web::all
 }
