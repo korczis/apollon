f52d5fce303ba5af283d75513c2bb112cdc4cf3a
Tomas Korcak <korczis@gmail.com>
2014-05-18 21:18:49 -0700

    Enable user management

--- puppet/modules/rabbitmq/manifests/init.pp
+++ puppet/modules/rabbitmq/manifests/init.pp
@@ -5,6 +5,18 @@
 class rabbitmq {
   require core
 
+  # create a directory for temporary files scripts
+  file { "/home/vagrant/scripts/rabbitmq":
+    ensure => "directory",
+  }
+
+  file { "/home/vagrant/scripts/rabbitmq/enable-user-management.sh":
+    owner => root,
+    group => root,
+    mode => 0755,
+    source  => 'puppet:///modules/rabbitmq/enable-user-management.sh';
+  }
+
   package { 'rabbitmq-server':
     ensure => present;
   }
@@ -12,5 +24,13 @@ class rabbitmq {
   service { 'rabbitmq-server':
     require => Package['rabbitmq-server'],
     ensure => running,
+  }
+
+  exec { 'enable-user-management':
+    require => [
+    	File['/home/vagrant/scripts/rabbitmq/enable-user-management.sh'],
+    	Service['rabbitmq-server']
+	],
+    command => '/home/vagrant/scripts/rabbitmq/enable-user-management.sh'
   }
 }
