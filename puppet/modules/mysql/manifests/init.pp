e317b5319d624c8a0c15dabf7641288e2afad116
Tomas Korcak <korczis@gmail.com>
2014-05-18 21:17:14 -0700

    Added mysql module

--- puppet/modules/mysql/manifests/init.pp
+++ puppet/modules/mysql/manifests/init.pp
@@ -8,4 +8,38 @@ class mysql {
   package { 'mysql-server':
     ensure => present;
   }
+
+  file { '/etc/mysql/debian.cnf':
+    source  => 'puppet:///modules/mysql/debian.cnf',
+    owner => root,
+    group => root,
+    require => Package['mysql-server'],
+    notify  => Service['mysql'];
+  }
+
+  file { '/etc/mysql/my.cnf':
+    source  => 'puppet:///modules/mysql/my.cnf',
+    owner => root,
+    group => root,
+    require => Package['mysql-server'],
+    notify  => Service['mysql'];
+  }
+  
+  file { '/etc/mysql/conf.d/mysqld_safe_syslog.cnf':
+    source  => 'puppet:///modules/mysql/conf.d/mysqld_safe_syslog.cnf',
+    owner => root,
+    group => root,
+    require => Package['mysql-server'],
+    notify  => Service['mysql'];
+  }
+
+  service { 'mysql':
+    ensure  => running,
+    require => [
+      Package['mysql-server'],
+      File['/etc/mysql/debian.cnf'],
+      File['/etc/mysql/my.cnf'],
+      File['/etc/mysql/conf.d/mysqld_safe_syslog.cnf']
+    ];
+  }
 }
