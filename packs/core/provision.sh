6bc65dcf628f0eaf2a2b78d5ffb17a7952d71b9c
Tomas Korcak <korczis@gmail.com>
2014-05-22 22:44:44 -0700

    Cosmetics ...

--- packs/core/provision.sh
+++ packs/core/provision.sh
@@ -2,6 +2,6 @@
 
 echo "Basic provisioning using bash script is happening right now ..."
 
-mount /dev/cdrom /mnt
-cd /mnt
-./VBoxLinuxAdditions.run
+# sudo mount /dev/cdrom /mnt
+# cd /mnt
+# sudo ./VBoxLinuxAdditions.run
