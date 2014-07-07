4f81fcf010ddae0dd25ba0672439be7bf662e1f7
Tomas Korcak <korczis@gmail.com>
2014-07-07 13:26:49 -0700

    Fix rubocop issues

--- lib/apollon/cli/app.rb
+++ lib/apollon/cli/app.rb
@@ -8,29 +8,31 @@ program_desc 'Apollon CLI'
 
 desc 'Build new web-unit'
 command :build do |c|
-  c.action do |global_options, options, args|
+  c.action do # |global_options, options, args|
     # TODO: Implement here
   end
 end
 
 desc 'Deploy built web-unit'
 command :deploy do |c|
-  c.action do |global_options, options, args|
+  c.action do # |global_options, options, args|
     # TODO: Implement here
   end
 end
 
 desc 'List deployed web-units'
 command :list do |c|
-  c.action do |global_options, options, args|
+  c.action do # |global_options, options, args|
     # TODO: Implement here
   end
 end
 
 module Apollon
+  # Apollon CLI
   module Cli
     class << self
       def run(argv = ARGV)
+        Kernel.send(:run, argv)
       end
     end
   end
