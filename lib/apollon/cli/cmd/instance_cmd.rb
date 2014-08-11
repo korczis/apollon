540acc0d8c9253738bd1982571f128716d72e43c
Tomas Korcak <korczis@gmail.com>
2014-08-11 00:54:11 -0700

    Torso for listing instances

---
+++ lib/apollon/cli/cmd/instance_cmd.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+require 'gli'
+require 'pp'
+require 'terminal-table'
+
+include GLI::App
+
+require_relative '../../instance/instance'
+
+require_relative '../shared'
+
+desc 'Instance'
+command :instance do |c|
+  c.desc 'List existing instances'
+  c.command :list do |list|
+    list.action do
+      data = Apollon::Instance.list.each_with_index.map do |item, i|
+        []
+      end
+
+      # TODO: Implement here
+      puts Terminal::Table.new title: 'Instance List',
+                               headings: %w(Id Name Class Path),
+                               rows: data
+    end
+  end
+end
---
+++ lib/apollon/instance/instance.rb
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+
+module Apollon
+  module Instance
+    class << self
+      def list
+        []
+      end
+    end
+  end
+end
