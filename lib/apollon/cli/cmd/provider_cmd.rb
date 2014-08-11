7977bc423ff1bfeb8153dbd3d17c288a0b901d47
Tomas Korcak <korczis@gmail.com>
2014-08-11 01:55:07 -0700

    Dynamic provider commands

--- lib/apollon/cli/cmd/provider_cmd.rb
+++ lib/apollon/cli/cmd/provider_cmd.rb
@@ -10,12 +10,16 @@ require_relative '../../provider/provider'
 
 require_relative '../shared'
 
+def providers
+  Apollon::Provider.list
+end
+
 desc 'Provider'
 command :provider do |c|
   c.desc 'List supported providers'
   c.command :list do |list|
     list.action do
-      data = Apollon::Provider.list.each_with_index.map do |item, i|
+      data = providers.each_with_index.map do |item, i|
         [i, item[:name], item[:klass], item[:path]]
       end
 
@@ -23,6 +27,18 @@ command :provider do |c|
       puts Terminal::Table.new title: 'Provider List',
                                headings: %w(Id Name Class Path),
                                rows: data
+    end
+  end
+
+  providers.each do |provider|
+    name = provider[:name]
+    klass = provider[:klass]
+    path = provider[:path]
+    c.desc "Provider #{klass}"
+    c.command name do |cmd|
+      cmd.action do
+        puts "Hello I am #{name} - #{klass} - #{path}"
+      end
     end
   end
 end
