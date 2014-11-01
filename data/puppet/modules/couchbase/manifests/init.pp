6f98f379299e351b5a1510d0d5c1c943cf23ceaa
Tomas Korcak <korczis@gmail.com>
2014-11-01 13:35:23 +0100

    Couchbase module

---
+++ data/puppet/modules/couchbase/manifests/init.pp
@@ -0,0 +1,24 @@
+# == Class: couchbase
+#
+# Installs development packages
+#
+class couchbase {
+  require core
+  require erlang
+
+  exec { "couchbase-download":
+    command => "/usr/bin/wget -P /home/vagrant/tmp http://packages.couchbase.com/releases/3.0.1/couchbase-server-community_3.0.1-ubuntu12.04_amd64.deb"
+  }
+
+  exec { "couchbase-install":
+    require => Exec["couchbase-download"],
+    command => "/usr/bin/dpkg -i /home/vagrant/tmp/couchbase-server-community_3.0.1-ubuntu12.04_amd64.deb"
+  }
+
+  service { 'couchbase':
+    ensure  => running,
+    require => [
+    Exec['couchbase-install']
+    ]
+  }
+}
