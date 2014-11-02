17da169de82e60c3d61784fe93aec72c9cf45445
Tomas Korcak <korczis@gmail.com>
2014-11-02 03:50:47 +0100

    Improved provisioning script

--- data/packer/core/scripts/provision-digitalocean.sh
+++ data/packer/core/scripts/provision-digitalocean.sh
@@ -2,10 +2,14 @@
 
 echo "Basic provisioning using bash script is happening right now ..."
 
+echo "Creating home directory"
 mkdir -p /home/apollon
 
+echo "Creating user and adding user to sudo group"
 useradd -m -U -G sudo -b /home/apollon -p apollon apollon
 
+echo "Changing ownership of home directory"
 chown -R apollon:apollon /home/apollon
 
+echo "Adding user to sudoers"
 echo "apollon   ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers; sync;
