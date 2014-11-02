d7b0c889e4de175b0ac04e7244cb450731e89331
Tomas Korcak <korczis@gmail.com>
2014-11-02 02:00:31 +0100

    Formatting

--- data/puppet/modules/dev/manifests/init.pp
+++ data/puppet/modules/dev/manifests/init.pp
@@ -3,18 +3,18 @@
 # Installs development packages
 #
 class dev::all {
-	include clang
-	include cmake
-	include erlang
-	include ghc
-	include git
-	include golang
-	include java
-	include llvm
-	include llvm
-	include nodejs
-	include php5
-	include python
-	#  include ruby
-	include rvm
+  include clang
+  include cmake
+  include erlang
+  include ghc
+  include git
+  include golang
+  include java
+  include llvm
+  include llvm
+  include nodejs
+  include php5
+  include python
+  #  include ruby
+  include rvm
 }
