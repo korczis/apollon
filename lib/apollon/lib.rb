4c15979935f2f782a06fffa18669dd7a76dcc24c
Tomas Korcak <korczis@gmail.com>
2015-01-18 16:30:33 +0100

    Added missing lib.rb

---
+++ lib/apollon/lib.rb
@@ -0,0 +1,19 @@
+# encoding: UTF-8
+
+require 'pathname'
+
+# require_relative 'cli/cli'
+
+base = Pathname(__FILE__).dirname.expand_path
+
+res = []
+
+Dir.glob(base + '*.rb').each do |file|
+  loaded_file = require file
+  res << {
+    :file => file,
+    :loaded => loaded_file
+  }
+end
+
+res
